c33997d300776f9bbf524219949c3d5f04516966
seolsnow <hanseolization@gmail.com>
2026-04-23 06:54:42 +0900

    Update ClaudeMeter to v0.2.4

--- Casks/claudemeter.rb
+++ Casks/claudemeter.rb
@@ -1,6 +1,6 @@
 cask "claudemeter" do
-  version "0.2.3"
-  sha256 "fa13e015a342348820cff9c8f7c0b4f0c5cc3813c923ad0737e64d57e7a46527"
+  version "0.2.4"
+  sha256 "3ce5f526c102e4b1965c4c0f32ed5acd9b27d8e0a93022816a32cb14bed70c0c"
 
   url "https://github.com/seolsnow/ClaudeMeter/releases/download/v#{version}/ClaudeMeter.zip"
   name "ClaudeMeter"
